debbb141ba927d363c47383aa21f2fee35dabd44
ClAndrew <cmlandrews@gmail.com>
2019-01-19 01:32:51 -0800

    Add solution and code

---
+++ CopyToInstall.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+:: This script is available as a convenience to deploy the plugin locally for testing as part of the build operation.
+
+:: Enable the copying behavior below
+:: set DEPLOY_PLUGIN_LOCALLY=1
+set DEPLOY_PLUGIN_LOCALLY=1
+if NOT "%DEPLOY_PLUGIN_LOCALLY%"=="1" goto :end
+
+:: Set the environment variables below as needed.
+set REPOSITORY_DIR=C:\Users\Claire\source\repos\DiscordRichPresence
+set BUILD_CONFIGURATION=Debug
+set WINAMP_INSTALL_DIR=E:\Program Files (x86)\Winamp
+set DISCORD_RPC_LIBRARY_DIR=C:\Users\Claire\source\repos\DiscordRichPresence\discord-rpc
+set COPY_SYMBOLS=0
+
+set BUILT_BINARY_DIR=%REPOSITORY_DIR%\%BUILD_CONFIGURATION%
+set WINAMP_PLUGINS_DIR=%WINAMP_INSTALL_DIR%\Plugins
+
+copy  /Y "%BUILT_BINARY_DIR%\DiscordRichPresence.dll" "%WINAMP_PLUGINS_DIR%\gen_DiscordRichPresence.dll"
+
+if NOT "%COPY_SYMBOLS%"=="1" goto :afterCopySymbols
+copy  /Y "%BUILT_BINARY_DIR%\DiscordRichPresence.pdb" "%WINAMP_PLUGINS_DIR%\gen_DiscordRichPresence.pdb"
+:afterCopySymbols
+
+mkdir "%WINAMP_PLUGINS_DIR%\DiscordRichPresence"
+copy /Y "%DISCORD_RPC_LIBRARY_DIR%\win32-dynamic\bin\*.*" "%WINAMP_PLUGINS_DIR%\DiscordRichPresence"
+
+:end
---
+++ DiscordRichPresence/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Config.rc
+//
+#define IDD_DIALOG_CONFIG               101
+#define IDC_CHECK_DISPLAY_TITLE_IN_STATUS 1002
+#define IDC_EDIT_DISCORD_APPLICATION_ID 1003
+#define IDC_BUTTON_APPLY                1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
