b2c4aa9cc3a7e1de41983b9672520ec124815bd3
ClAndrew <cmlandrews@gmail.com>
2019-06-27 22:03:23 -0700

    Update CreateRelease.cmd script to do more things automatically

--- CreateRelease.cmd
+++ CreateRelease.cmd
@@ -3,8 +3,16 @@ setlocal
 
 set ZIP_UTILITY="E:\Program Files\7-Zip\7z.exe"
 
+del Distribute.zip
+rmdir /S /Q Distribute
 
 mkdir Distribute
+mkdir Distribute\DiscordRichPresence
+
+copy DiscordRuntime\discord-rpc.dll Distribute\DiscordRichPresence\discord-rpc.dll
+copy DiscordRuntime\send-presence.exe Distribute\DiscordRichPresence\send-presence.exe
+copy settings.ini Distribute\DiscordRichPresence\settings.ini
 copy Release\DiscordRichPresence.dll Distribute\gen_DiscordRichPresence.dll
+
 
 %ZIP_UTILITY% a Distribute.zip Distribute\
