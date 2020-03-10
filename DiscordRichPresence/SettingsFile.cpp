36d74cb0d412e8a705a990b357f2a74eba5a1fa4
Derek Gates <derek@derekgates.us>
2020-03-10 00:54:48 -0500

    Remove unused s2ws function, fix formatting

--- DiscordRichPresence/SettingsFile.cpp
+++ DiscordRichPresence/SettingsFile.cpp
@@ -11,22 +11,6 @@ std::string GetExecutableFileName()
 	return std::string(buffer);
 }
 
-//used to convert strings to a usable string in MessageBox, example:
-//std::wstring stemp = s2ws(executablePath);
-//MessageBox(0, stemp.c_str(), L"", MB_OK);
-//https://stackoverflow.com/a/27296
-std::wstring s2ws(const std::string& s)
-{
-    int len;
-    int slength = (int)s.length() + 1;
-    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0); 
-    wchar_t* buf = new wchar_t[len];
-    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
-    std::wstring r(buf);
-    delete[] buf;
-    return r;
-}
-
 std::string GetExecutablePath()
 {
 	std::string executableFileName = GetExecutableFileName();
